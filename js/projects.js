f6bfc8ce79a34df0f89b06053eb09990aae6db94
Daniil <88019338+EduTutor@users.noreply.github.com>
2023-01-15 16:11:31 -0500

    first commit

---
+++ js/projects.js
@@ -0,0 +1,86 @@
+var Regression_Analysis_AWS =
+'<div class="project p-light" id="regression">' +
+'<a class="overlay" href="https://github.com/JoexTitan/Regression-Analysis-AWS" target="_blank">' +
+'<div class="content vertical-center">' +
+'<h4>JoexTitan</h4>' +
+'<h1>Regression<br>Analysis ON AWS</h1>' +
+'<p>Data pipeline that orchestrates a batch ETL process within Amazon EMR<br>in order to provide a predictive estimate for the employee compensation range.</p>' +
+'<button>Explore <span class="arrow"></span></button>' +
+'</div>' +
+'</a>' +
+'</div>';
+
+var tw_sentiment =
+'<div class="project p-light" id="sentiment">' +
+'<a class="overlay" href="https://github.com/JoexTitan/Twitter-Data-Sentiment-Analysis" target="_blank">' +
+'<div class="content vertical-center">' +
+'<h4>JoexTitan</h4>' +
+'<h1>Twitter<br>Data<br>Pipeline</h1>' +
+'<p>Extracting the sentiment from twitter data in order to deliver<br>insights for yearly activity, the findings are presented with Power BI.</p>' +
+'<button>Explore <span class="arrow"></span></button>' +
+'</div>' +
+'</a>' +
+'</div>';
+
+var tw_pipeline =
+'<div class="project p-light" id="pipeline">' +
+'<a class="overlay" href="https://github.com/JoexTitan/Social-Media-ETL-Pipeline" target="_blank">' +
+'<div class="content vertical-center">' +
+'<h4>JoexTitan</h4>' +
+'<h1>Slack Bot<br>Sentiment<br>Automation</h1>' +
+'<p>ETL pipeline that extracts data from Twitter API, performs<br>sentiment analysis, and shares the tweets via Slack channel.</p>' +
+'<button>Explore <span class="arrow"></span></button>' +
+'</div>' +
+'</a>' +
+'</div>';
+
+
+var loan_prediction =
+'<div class="project p-light" id="predictive_modeling">' +
+'<a class="overlay" href="https://github.com/JoexTitan/RFLP-Prediction-Model" target="_blank">' +
+'<div class="content vertical-center">' +
+'<h4>JoexTitan</h4>' +
+'<h1>supervised<br>machine learning</h1>' +
+'<p>A Classification model in which we predict whether or not<br>a loan will be approved based on candidate\'s metadata.</p>' +
+'<button>Explore <span class="arrow"></span></button>' +
+'</div>' +
+'</a>' +
+'</div>';
+
+
+var song_reccomendation =
+'<div class="project p-white" id="spotify">' +
+'<a class="overlay" href="https://github.com/JoexTitan/Spotify-Recommendation-App" target="_blank">' +
+'<div class="content vertical-center">' +
+'<h4>JoexTitan</h4>' +
+'<h1>spotify song<br>analytics platform</h1>' +
+'<p>This application analyzes your music tastes and provides you with a unique song reccomendation based on audio metrics, tempo, loudness, and much more...</p>' +
+'<button>Explore <span class="arrow"></span></button>' +
+'</div>' +
+'</a>' +
+'</div>';
+
+var object_detection =
+'<div class="project p-light" id="detection">' +
+'<a class="overlay" href="https://github.com/JoexTitan/Object-Detection-Model" target="_blank">' +
+'<div class="content vertical-center">' +
+'<h4>JoexTitan</h4>' +
+'<h1>Object Detection AI</h1>' +
+'<p>The following machine learning model aims to identify objects<br>in the video and predict the trajecory that they are moving in.</p>' +
+'<button>Explore <span class="arrow"></span></button>' +
+'</div>' +
+'</a>' +
+'</div>';
+
+
+var featured = [
+    song_reccomendation,
+    tw_sentiment
+];
+
+var all = [
+    Regression_Analysis_AWS,
+    tw_pipeline,
+    loan_prediction,
+    object_detection
+];
